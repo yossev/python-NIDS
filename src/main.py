ee38f8749f1d9c1420b3337efc6b8df1539a63f3
Yossef Hisham <synthpyt@gmail.com>
2024-08-25 11:46:13 +0300

    - Added Options for the user

--- src/main.py
+++ src/main.py
@@ -4,11 +4,44 @@ from feature_extraction  import save_features_to_csv
 
 def main():
     packets =  sniff_packets()
-    print(packets)
+    ascii = """
+    _..-'(                       )`-.._
+                   ./'. '||\\.       (\_/)       .//||` .`\.
+                ./'.|'.'||||\\|..    )O O(    ..|//||||`.`|.`\.
+             ./'..|'.|| |||||\`````` '`"'` ''''''/||||| ||.`|..`\.
+           ./'.||'.|||| ||||||||||||.     .|||||||||||| |||||.`||.`\.
+          /'|||'.|||||| ||||||||||||{     }|||||||||||| ||||||.`|||`\
+         '.|||'.||||||| ||||||||||||{     }|||||||||||| |||||||.`|||.`
+        '.||| ||||||||| |/'   ``\||``     ''||/''   `\| ||||||||| |||.`
+        |/' \./'     `\./         \!|\   /|!/         \./'     `\./ `\|
+        V    V         V          }' `\ /' `{          V         V    V
+        `    `         `               V               '         '    '
+    """
 
-    df = process_packets(packets)
+    print(ascii)
 
-    save_features_to_csv(df, 'packet_features.csv')
+
+    input("Welcome to batds IDS")
+    print("--help for the list of commands")
+    while True:
+        command = input("batds> ")
+        if command == "--help":
+            print("Commands:")
+            print("--exit: Exit the program")
+            print("--show: Show the features of the packets")
+            print("--save: Save the features of the packets to a csv file")
+        elif command == "--exit":
+            break
+        elif command == "--show":
+            df = process_packets(packets)
+            print("Processing packets...")
+            print("Showing features of the packets")
+            print(df)
+        elif command == "--save":
+            print("Saving features of the packets to a csv file")
+            save_features_to_csv(df, 'packet_features.csv')
+        else:
+            print("Invalid command")
 
 
 
